77bffa4f83e2a386414719191bed2174e32ac4b2
MatsumuraYuki <define1394@gmail.com>
2016-04-06 01:44:45 +0900

    enclosed test added

--- app/src/test/java/yuki/m/android/now/time/TimeTest.java
+++ app/src/test/java/yuki/m/android/now/time/TimeTest.java
@@ -18,6 +18,8 @@ package yuki.m.android.now.time;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
 import yuki.m.android.now.time.DateTimeRule.Now;
 
@@ -27,57 +29,82 @@ import static yuki.m.android.now.time.Time.parseIso8601z;
 /**
  * Created by YukiMatsumura on 16/04/06.
  */
+@RunWith(Enclosed.class)
 public class TimeTest {
 
-    @Rule
-    public DateTimeRule dateTimeRule = new DateTimeRule();
+    public static class DateTimeRuleで現在日時を固定化しないケース {
 
-    @Test
-    public void CurrentTime_Now指定なし() throws Exception {
-        // @Nowの日時指定がない＆DateTimeRuleの引数なし初期化で現在日時が固定化されていないことを試験する.
-        // 厳密には現在日時が2000-01-01T00:00:00Zを指す可能性はあるが、
-        // 過去の日時が現在の日時と恒久的に等しくならない性質から, 下記で良しとする.
-        assertThat(Time.now()).isNotEqualTo(parseIso8601z("2000-01-01T00:00:00Z"));
+        @Rule
+        public DateTimeRule dateTimeRule = new DateTimeRule();
+
+        @Test
+        public void CurrentTime_Now指定なし() throws Exception {
+            // @Nowの日時指定がない＆DateTimeRuleの引数なし初期化で現在日時が固定化されていないことを試験する.
+            // 厳密には現在日時が2000-01-01T00:00:00Zを指す可能性はあるが、
+            // 過去の日時が現在の日時と恒久的に等しくならない性質から, 下記で良しとする.
+            assertThat(Time.now()).isNotEqualTo(parseIso8601z("2000-01-01T00:00:00Z"));
+        }
+
+        @Test
+        @Now("2000-01-01T00:00:00Z")
+        public void CurrentTime_Now指定あり() throws Exception {
+            // @Nowの日時で現在日時が固定化されていることを試験する.
+            // 厳密には現在日時が2000-01-01T00:00:00Zを指す可能性はあるが、
+            // 過去の日時が現在の日時と恒久的に等しくならない性質から, 下記で良しとする.
+            assertThat(Time.now()).isEqualTo(parseIso8601z("2000-01-01T00:00:00Z"));
+        }
+
+        @Test
+        @Now("2000-01-02T00:00:00Z")
+        public void 日時加算_Now指定あり() throws Exception {
+            // @Nowで指定した日時の13日後の日時が計算されることを試験する
+            assertThat(Time.afterDays(13)).isEqualTo(parseIso8601z("2000-01-15T00:00:00Z"));
+        }
+
+        @Test
+        public void 日時加算_Now指定なし() throws Exception {
+            // アノテーション指定無しで13日後の日時が計算されることを試験する.
+            // このテストケースは現在日時が"2000-01-01T00:00:00Z"で固定化されていないことを期待する.
+            assertThat(Time.afterDays(13)).isNotEqualTo(parseIso8601z("2000-01-13T00:00:00Z"));
+            assertThat(Time.afterDays(13)).isNotEqualTo(parseIso8601z("2000-01-14T00:00:00Z"));
+        }
     }
+    public static class DateTimeRuleで現在日時を固定化するケース {
 
-    @Test
-    @Now("2000-01-01T00:00:00Z")
-    public void CurrentTime_Now指定あり() throws Exception {
-        // @Nowの日時で現在日時が固定化されていることを試験する.
-        // 厳密には現在日時が2000-01-01T00:00:00Zを指す可能性はあるが、
-        // 過去の日時が現在の日時と恒久的に等しくならない性質から, 下記で良しとする.
-        assertThat(Time.now()).isEqualTo(parseIso8601z("2000-01-01T00:00:00Z"));
-    }
+        @Rule
+        public DateTimeRule dateTimeRule = new DateTimeRule("2000-02-01T00:00:00Z");
 
-    @Test
-    @Now("2000-01-02T00:00:00Z")
-    public void 日時加算1() throws Exception {
-        // @Nowで指定した日時の13日後の日時が計算されることを試験する
-        // この試験は他のテストケースで試験されたNowの効果が継続していないことも確認する
-        assertThat(Time.afterDays(13)).isEqualTo(parseIso8601z("2000-01-15T00:00:00Z"));
-    }
+        @Test
+        public void CurrentTime_Now指定なし() throws Exception {
+            // @Nowの日時指定がない＆DateTimeRuleの引数あり初期化で現在日時が固定化されていることを試験する.
+            // 厳密には現在日時が2000-02-01T00:00:00Zを指す可能性はあるが、
+            // 過去の日時が現在の日時と恒久的に等しくならない性質から, 下記で良しとする.
+            assertThat(Time.now()).isEqualTo(parseIso8601z("2000-02-01T00:00:00Z"));
+        }
 
-    @Test
-    public void 日時加算2() throws Exception {
-        // アノテーション指定無しで13日後の日時が計算されることを試験する.
-        // このテストケースはNowRuleが"2000-01-01T00:00:00Z"ので初期化されていることを期待する.
-        // この試験は他のテストケースで試験されたNowの効果が継続していないことも確認する
-        assertThat(Time.afterDays(13)).isEqualTo(parseIso8601z("2000-01-14T00:00:00Z"));
-    }
+        @Test
+        @Now("2000-03-01T00:00:00Z")
+        public void CurrentTime_Now指定あり() throws Exception {
+            // @Nowの日時で現在日時が固定化されていることを試験する.
+            // 厳密には現在日時が2000-03-01T00:00:00Zを指す可能性はあるが、
+            // 過去の日時が現在の日時と恒久的に等しくならない性質から, 下記で良しとする.
+            assertThat(Time.now()).isEqualTo(parseIso8601z("2000-03-01T00:00:00Z"));
+        }
 
-    @Test
-    @Now("2000-01-03T00:00:00Z")
-    public void 日時加算3() throws Exception {
-        // @Nowで指定した日時の13日後の日時が計算されることを試験する
-        // この試験は他のテストケースで試験されたNowの効果が継続していないことも確認する
-        assertThat(Time.afterDays(13)).isEqualTo(parseIso8601z("2000-01-16T00:00:00Z"));
-    }
+        @Test
+        @Now("2000-03-02T00:00:00Z")
+        public void 日時加算_Now指定あり() throws Exception {
+            // @Nowで指定した日時の13日後の日時が計算されることを試験する
+            // このテストケースは現在日時が@Nowにより"2000-03-01T00:00:00Z"で固定化されていることを期待する.
+            assertThat(Time.afterDays(13)).isEqualTo(parseIso8601z("2000-03-15T00:00:00Z"));
+        }
 
-    @Test
-    public void 日時加算4() throws Exception {
-        // @Nowで指定した日時の13日後の日時が計算されることを試験する.
-        // このテストケースはNowRuleが"2000-01-01T00:00:00Z"ので初期化されていることを期待する.
-        // この試験は他のテストケースで試験されたNowの効果が継続していないことも確認する
-        assertThat(Time.afterDays(13)).isEqualTo(parseIso8601z("2000-01-14T00:00:00Z"));
+        @Test
+        public void 日時加算_Now指定なし() throws Exception {
+            // アノテーション指定無しで13日後の日時が計算されることを試験する.
+            // このテストケースは現在日時がDateTimeRuleの初期化により"2000-02-01T00:00:00Z"で
+            // 固定化されていることを期待する.
+            assertThat(Time.afterDays(13)).isEqualTo(parseIso8601z("2000-02-14T00:00:00Z"));
+        }
     }
 }
